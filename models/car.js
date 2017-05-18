74db5961d13464da033dbc09f9a31c91cd0d5f22
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-05-18 17:07:15 +0700

    first-step'

---
+++ models/car.js
@@ -0,0 +1,20 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Car = sequelize.define('Car', {
+    name: DataTypes.STRING,
+    manufacture: DataTypes.STRING,
+    color: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    status: DataTypes.BOOLEAN,
+    description: DataTypes.STRING,
+    image: DataTypes.STRING,
+    category_id: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Car;
+};
---
+++ models/transaction.js
@@ -0,0 +1,16 @@
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var Transaction = sequelize.define('Transaction', {
+    start_date: DataTypes.DATE,
+    finish_date: DataTypes.DATE,
+    driver: DataTypes.BOOLEAN,
+    car_id: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return Transaction;
+};

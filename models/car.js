0742bb76ded1545abd7325ad55824557da44c1bd
DeriKurniawan <derikurniawan11d88@gmail.com>
2017-05-18 19:12:13 +0700

    creating routers-cars-categories

--- models/car.js
+++ models/car.js
@@ -13,6 +13,8 @@ module.exports = function(sequelize, DataTypes) {
     classMethods: {
       associate: function(models) {
         // associations can be defined here
+        Car.belongsTo(models.Category, {foreignKey: 'category_id'})
+        Car.hasOne(models.Transaction, {foreignKey: 'car_id'})
       }
     }
   });
